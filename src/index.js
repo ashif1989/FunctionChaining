d3c8fa79f6a2e2f6c88ebfd37a964ecb55d9a75d
Mohamed Ashif Ali Jamal Mohamed <55224952+ashif1989@users.noreply.github.com>
2023-05-24 17:03:32 +0530

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,32 @@
+let calc = (num) => {
+  let result = num;
+
+  let add = (int) => {
+    result += int;
+    return calc(result);
+  };
+
+  let sub = (int) => {
+    result -= int;
+    return calc(result);
+  };
+
+  let mul = (int) => {
+    result *= int;
+    return calc(result);
+  };
+
+  let div = (int) => {
+    result /= int;
+    return calc(result);
+  };
+
+  let get = () => {
+    return result;
+  };
+
+  return { add, sub, mul, div, get };
+};
+
+let chainFunction = calc(2).add(2).sub(1).mul(4).div(2).get();
+document.write(chainFunction);
